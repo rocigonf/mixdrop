392ffce50eabffdb167c6d086921a2e771ac92c1
Rocío Gonfaus Luengo <116120484+rocigonf@users.noreply.github.com>
2025-01-10 21:09:21 +0100

    Oh the misery
    Creado back con plantilla correcta :(

--- mixdrop-back/mixdrop-back/Program.cs
+++ mixdrop-back/mixdrop-back/Program.cs
@@ -1,41 +1,34 @@
+
 namespace mixdrop_back
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            // Estas 3 variables habrá que ponerlas en otro lado
-            var semitone = Math.Pow(2, 1.0 / 12);
-            var upOneTone = semitone * semitone;
-            var downOneTone = 1.0 / upOneTone;
-
-            Console.WriteLine("Procesando...");
-            HellIsForever.ChangeBPM("music.wav", "output.wav", 2.0f);
-            Console.WriteLine("Procesado");
-
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddRazorPages();
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
+            if (app.Environment.IsDevelopment())
             {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
 
             app.UseAuthorization();
 
-            app.MapRazorPages();
+
+            app.MapControllers();
 
             app.Run();
         }
