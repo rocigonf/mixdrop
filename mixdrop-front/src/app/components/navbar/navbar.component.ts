49007028bf45a5d885de581f72f8d6e17af2e0ef
maria99r <maria99r@hotmail.com>
2025-01-12 21:42:21 +0100

    nav imagen avatar

--- mixdrop-front/src/app/components/navbar/navbar.component.ts
+++ mixdrop-front/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { environment } from '../../../environments/environment';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-navbar',
@@ -7,6 +10,17 @@ import { Component, Input } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
   @Input() frameName = "/images/speaker/speaker1.png"
+
+  constructor(public authService: AuthService){}
+
+  user: User | null = null;
+
+  public readonly IMG_URL = environment.apiImg;
+
+  ngOnInit() {
+    // usuario logueado
+    this.user = this.authService.getUser();
+  }
 }
