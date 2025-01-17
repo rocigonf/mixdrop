00c77addbf8e96fc54fec61d52cb69a3e2a13280
maria99r <maria99r@hotmail.com>
2025-01-17 19:54:10 +0100

    lista de usuarios - proceso -

--- mixdrop-front/src/app/pages/menu/menu.component.ts
+++ mixdrop-front/src/app/pages/menu/menu.component.ts
@@ -6,11 +6,14 @@ import { MessageType } from '../../models/message-type';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
 import { UserService } from '../../services/user.service';
 import { User } from '../../models/user';
+import { FormsModule } from '@angular/forms';
+import { environment } from '../../../environments/environment';
+
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FormsModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
@@ -18,6 +21,14 @@ export class MenuComponent implements OnInit, OnDestroy {
   messageReceived$: Subscription | null = null;
   serverResponse: string = '';
   totalPlayers = 0;
+
+  searchedUsers!: User[];
+
+  query: string = '';
+
+  public readonly IMG_URL = environment.apiImg;
+
+
   
   constructor (private webSocketService : WebsocketService, private router: Router, private userService :UserService){}
 
@@ -56,7 +67,9 @@ export class MenuComponent implements OnInit, OnDestroy {
 
   async getSearchedUsers(query: string) : Promise<User[]> {
     const result = await this.userService.searchUser(query);
+    console.log(result)
+    this.searchedUsers = result;
     return result;
   }
-  
+
 }
