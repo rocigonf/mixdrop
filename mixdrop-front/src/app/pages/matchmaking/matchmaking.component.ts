7c34ca2941cf28367ad8fa9a03c229ac221698df
moguism <148863935+moguism@users.noreply.github.com>
2025-01-29 19:50:37 +0100

    take me to snurch

--- mixdrop-front/src/app/pages/matchmaking/matchmaking.component.ts
+++ mixdrop-front/src/app/pages/matchmaking/matchmaking.component.ts
@@ -45,6 +45,57 @@
     </div>
     }
     }
+    console.log("Respuesta del socket en JSON: ", jsonResponse)
+  }
 
-    <hr>
-    <p>si es invitado esta esperando a q empiece el juego</p>
+  askForInfo(messageType: MessageType) {
+    console.log("Mensaje pedido: ", messageType)
+    this.webSocketService.sendRxjs(messageType.toString())
+  }
+
+
+  gameWithBot() {
+    // el user 2 es nulo y el false de que no es random
+    this.battleService.createBattle(null, false)
+  }
+
+  gameWithFriend(friend: User) {
+    this.battleService.createBattle(friend, false)
+  }
+
+  gameRandom() {
+    this.battleService.randomBattle()
+    console.log("mortadela");
+  }
+
+  // comprueba q el usuario ya tiene una solicitud de batalla pendiente con otro
+  hasBattle(user: User | null): boolean {
+    const has: boolean = this.pendingBattles.some(battle =>
+      (battle.user.id === user?.id) || (battle.user.id === this.user?.id)
+      || (battle.battleUsers[0].id === this.user?.id)  || (battle.battleUsers[1].id === this.user?.id) 
+    );
+    return has;
+  }
+
+
+  processFriends() {
+
+    this.myFriends = []
+    this.conenctedFriends = []
+
+    console.log(this.friendsRaw)
+
+    for (const friend of this.friendsRaw) {
+      if (friend.accepted) {
+        this.myFriends.push(friend)
+
+        if (friend.senderUser?.stateId == 2 || friend.receiverUser?.stateId == 2) {
+
+          if (friend.senderUser) this.conenctedFriends.push(friend.senderUser)
+          else if (friend.receiverUser) this.conenctedFriends.push(friend.receiverUser)
+        }
+      }
+    }
+    console.log("amigos: ", this.myFriends)
+  }
+}
