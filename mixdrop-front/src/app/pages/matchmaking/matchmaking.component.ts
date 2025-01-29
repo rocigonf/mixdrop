b5cc6e679a9e1e22486c059fb2f3b2ec04b4b982
moguism <148863935+moguism@users.noreply.github.com>
2025-01-29 19:50:48 +0100

    Update matchmaking.component.ts

--- mixdrop-front/src/app/pages/matchmaking/matchmaking.component.ts
+++ mixdrop-front/src/app/pages/matchmaking/matchmaking.component.ts
@@ -1,49 +1,74 @@
-<app-navbar></app-navbar>
+import { Component, OnInit } from '@angular/core';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { environment } from '../../../environments/environment';
+import { WebsocketService } from '../../services/websocket.service';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { UserService } from '../../services/user.service';
+import { User } from '../../models/user';
+import { BattleService } from '../../services/battle.service';
+import { Subscription } from 'rxjs';
+import { MessageType } from '../../models/message-type';
+import { Friend } from '../../models/friend';
+import { Battle } from '../../models/battle';
 
-<div class="cabecera">
-    <img [src]="IMG_URL + user?.avatarPath" alt="avatar de usuario">
-    <span style="font-size: 2rem;">{{user?.nickname}}</span> <!-- nickname del usuario-->
-</div>
+@Component({
+  selector: 'app-matchmaking',
+  standalone: true,
+  imports: [NavbarComponent],
+  templateUrl: './matchmaking.component.html',
+  styleUrl: './matchmaking.component.css'
+})
+export class MatchmakingComponent implements OnInit {
 
-<!--mostrar tamb avatar de invitado / anfitrion-->
+  user: User | null = null;
+  public readonly IMG_URL = environment.apiImg;
 
-<p>si es anitrion</p>
-<button (click)="gameWithBot()">Jugar contra un bot</button>
-<button (click)="gameRandom()">Jugar con oponente aleatorio</button>
-
-<!-- q te abra ventana model con amigos conectados ? -->
-<p>Invitar amigo a jugar</p>
-<div class="listaUsuarios"></div>
-@if (conenctedFriends.length < 1) { <span> No hay amigos conectados </span>
-    } @else {
-    @for (friend of conenctedFriends; track $index) {
+  constructor(private webSocketService: WebsocketService,
+    private router: Router,
+    private userService: UserService,
+    public authService: AuthService,
+    public battleService: BattleService
+  ) { }
 
 
-        <div class="tarjetaUsuario">
-            <div class="contenedorAvatar">
-                <img class="avatar" [src]="IMG_URL + friend.avatarPath" alt="usuario avatar" width="50px">
-            </div>
-    
-            <span class="nickname">
-                {{ friend.nickname }}
-            </span>
+  messageReceived$: Subscription | null = null;
+  serverResponse: string = '';
 
-            <div class="contenedorIcono">
-                <img class="icono" (click)="gameWithFriend(friend)" src="/images/addFriend.webp"
-                    alt="invitar a jugar a amigo">
-            </div>
+  friendsRaw: Friend[] = []
+  myFriends: Friend[] = []
+  conenctedFriends: User[] = []
 
-        </div>
+  pendingBattles: Battle[] = []
 
-        <span class="nickname">
-            {{ friend.nickname }}
-        </span>
+  ngOnInit(): void {
+    this.user = this.authService.getUser();
 
-        <div class="contenedorIcono">
-            <img class="icono" (click)="gameWithFriend(friend)" src="/images/battle.png" alt="invitar a jugar a amigo">
-        </div>
-    </div>
-    }
+    this.messageReceived$ = this.webSocketService.messageReceived.subscribe(message => this.processMessage(message))
+
+    // pide info de amigos 
+    this.askForInfo(MessageType.Friend)
+    this.askForInfo(MessageType.PendingBattle)
+    this.processFriends()
+  }
+
+  processMessage(message: any) {
+    this.serverResponse = message
+    const jsonResponse = JSON.parse(this.serverResponse)
+
+    switch (jsonResponse.messageType) {
+      case MessageType.Play:
+        // TODO: Redirigir a la vista (por ruta se pasa el id de la batalla)
+        alert("Partida encontrada :3")
+        this.router.navigateByUrl("game") // Redirigir a la vista juego
+        break
+      case MessageType.Friend:
+        this.friendsRaw = jsonResponse.friends
+        this.processFriends()
+        break
+      case MessageType.PendingBattle:
+        this.pendingBattles = jsonResponse.battles
+        break
     }
     console.log("Respuesta del socket en JSON: ", jsonResponse)
   }
